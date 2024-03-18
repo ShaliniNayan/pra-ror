95e971080f7f04b695494cc15a15f118e57ba408
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-03-18 20:21:17 +0530

    Generate Admin devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
 end

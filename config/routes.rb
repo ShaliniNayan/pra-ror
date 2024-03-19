ee4a819dca02a730f3c738c76e63040b3ad730ad
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-03-19 20:16:39 +0530

    Update category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

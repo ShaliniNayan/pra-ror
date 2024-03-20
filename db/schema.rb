14ac4cccb1ff209b9ab26010b88add13183545d4
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-03-20 22:33:49 +0530

    Add order product to fetch all the order & product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_20_135714) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_20_165833) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,6 +58,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_20_135714) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.string "size"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "customer_email"
     t.boolean "fulfilled"
@@ -89,6 +100,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_20_135714) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "products", "categories"
   add_foreign_key "stocks", "products"
 end
